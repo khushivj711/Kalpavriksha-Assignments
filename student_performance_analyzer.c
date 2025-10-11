86c63a7fb56160e75d6e9eff0d221627d8e1f47a
Khushi Vijay <khushi.vijay8711@gmail.com>
2025-10-11 15:39:14 +0530

    Update student_performance_analyzer.c
    i have done with all the updates.

--- student_performance_analyzer.c
+++ student_performance_analyzer.c
@@ -1,166 +1,186 @@
 #include <stdio.h>
 #include <stdbool.h>
 
-// Structure to store student details
+#define PASS_MARKS 35
+#define NUM_SUBJECTS 3
+
+#define GRADE_A_THRESHOLD 85
+#define GRADE_B_THRESHOLD 70
+#define GRADE_C_THRESHOLD 50
+
+#define STARS_GRADE_A 5
+#define STARS_GRADE_B 4
+#define STARS_GRADE_C 3
+#define STARS_GRADE_D 2
+#define STARS_GRADE_F 0
+
+typedef enum
+{
+  GRADE_A = 'A',
+  GRADE_B = 'B',
+  GRADE_C = 'C',
+  GRADE_D = 'D',
+  GRADE_F = 'F'
+} Grade;
+
 typedef struct
 {
-  char name[50];         // Student name
-  int rollNo;            // Roll number
-  float subjectMarks[3]; // Marks of 3 subjects
-  float totalMarks;      // Total of all subjects
-  float averageMarks;    // Average of marks
-  char grade;            // Grade based on average
+  char name[50];
+  int rollNo;
+  float subjectMarks[NUM_SUBJECTS];
 } StudentDetails;
 
-// Function declarations
 void processStudentPerformance();
-float calculateTotalMarks(float marks1, float marks2, float marks3);
+float calculateTotalMarks(StudentDetails student);
 float calculateAverageMarks(float totalMarks);
-char assignGrade(float averageMarks);
-void displayPerformancePattern(char grade);
+Grade assignGrade(float averageMarks);
+void displayPerformancePattern(Grade grade);
 void printRollNumbersRecursively(StudentDetails students[], int index, int totalStudents);
 bool isValidStudentCount(int studentCount);
 bool isValidMarks(float marks[]);
+void displayStudentPerformance(StudentDetails student);
 
 int main()
 {
-  processStudentPerformance(); // Run main logic
+  processStudentPerformance();
   return 0;
 }
 
-// Handles entire student performance process
 void processStudentPerformance()
 {
   int noOfStudents;
   printf("Enter Number of Students: ");
   scanf("%d", &noOfStudents);
-  getchar(); // To consume leftover newline
+  getchar();
 
   if (!isValidStudentCount(noOfStudents))
   {
-    // Validate number of students
     printf("Invalid number of students! Please enter between 1 and 100.\n");
     return;
   }
 
-  StudentDetails students[noOfStudents]; // Array to store student records
+  StudentDetails students[noOfStudents];
 
   for (int i = 0; i < noOfStudents; i++)
   {
-    // Input student data
     scanf("%d %s %f %f %f", &students[i].rollNo, students[i].name, &students[i].subjectMarks[0], &students[i].subjectMarks[1], &students[i].subjectMarks[2]);
 
     if (!isValidMarks(students[i].subjectMarks))
     {
-      // Validate entered marks
       printf("Invalid marks! Please enter values between 0 and 100.\n");
       return;
     }
-
-    // Calculate total, average, and grade
-    students[i].totalMarks = calculateTotalMarks(students[i].subjectMarks[0], students[i].subjectMarks[1], students[i].subjectMarks[2]);
-    students[i].averageMarks = calculateAverageMarks(students[i].totalMarks);
-    students[i].grade = assignGrade(students[i].averageMarks);
   }
 
   printf("\n");
 
   for (int i = 0; i < noOfStudents; i++)
   {
-    // Display student details
-    printf("Roll: %d\n", students[i].rollNo);
-    printf("Name: %s\n", students[i].name);
-    printf("Total: %.0f\n", students[i].totalMarks);
-    printf("Average: %.2f\n", students[i].averageMarks);
-    printf("Grade: %c\n", students[i].grade);
-
-    // Skip performance pattern if failed
-    if (students[i].averageMarks < 35)
-    {
-      printf("\n");
-      continue;
-    }
-
-    printf("Performance: ");
-    displayPerformancePattern(students[i].grade);
-    printf("\n\n");
+    displayStudentPerformance(students[i]);
   }
 
-  // Print roll numbers recursively
+  int startIndex = 0;
   printf("List of Roll Numbers (via recursion): ");
-  printRollNumbersRecursively(students, 0, noOfStudents);
+  printRollNumbersRecursively(students, startIndex, noOfStudents);
+  printf("\n");
 }
 
-// Validate student count (1 to 100)
+void displayStudentPerformance(StudentDetails student)
+{
+  float totalMarks = calculateTotalMarks(student);
+  float averageMarks = calculateAverageMarks(totalMarks);
+  Grade grade = assignGrade(averageMarks);
+
+  printf("Roll: %d\n", student.rollNo);
+  printf("Name: %s\n", student.name);
+  printf("Total: %.0f\n", totalMarks);
+  printf("Average: %.2f\n", averageMarks);
+  printf("Grade: %c\n", grade);
+
+  if (averageMarks >= PASS_MARKS)
+  {
+    printf("Performance: ");
+    displayPerformancePattern(grade);
+  }
+
+  printf("\n\n");
+}
+
 bool isValidStudentCount(int studentCount)
 {
   return !(studentCount < 1 || studentCount > 100);
 }
 
-// Validate each subject mark (0 to 100)
 bool isValidMarks(float marks[])
 {
-  return !(marks[0] < 0 || marks[0] > 100 || marks[1] < 0 || marks[1] > 100 || marks[2] < 0 || marks[2] > 100);
+  for (int i = 0; i < NUM_SUBJECTS; i++)
+  {
+    if (marks[i] < 0 || marks[i] > 100)
+    {
+      return false;
+    }
+  }
+  return true;
 }
 
-// Calculate total marks
-float calculateTotalMarks(float marks1, float marks2, float marks3)
+float calculateTotalMarks(StudentDetails student)
 {
-  return marks1 + marks2 + marks3;
+  float total = 0;
+  for (int i = 0; i < NUM_SUBJECTS; i++)
+  {
+    total += student.subjectMarks[i];
+  }
+  return total;
 }
 
-// Calculate average marks
 float calculateAverageMarks(float totalMarks)
 {
-  return totalMarks / 3;
+  return totalMarks / NUM_SUBJECTS;
 }
 
-// Assign grade based on average marks
-char assignGrade(float averageMarks)
+Grade assignGrade(float averageMarks)
 {
-  if (averageMarks >= 85)
+  if (averageMarks >= GRADE_A_THRESHOLD)
   {
-    return 'A';
+    return GRADE_A;
   }
-  else if (averageMarks < 85 && averageMarks >= 70)
+  else if (averageMarks >= GRADE_B_THRESHOLD)
   {
-    return 'B';
+    return GRADE_B;
   }
-  else if (averageMarks < 70 && averageMarks >= 50)
+  else if (averageMarks >= GRADE_C_THRESHOLD)
   {
-    return 'C';
+    return GRADE_C;
   }
-  else if (averageMarks < 50 && averageMarks >= 35)
+  else if (averageMarks >= PASS_MARKS)
   {
-    return 'D';
+    return GRADE_D;
   }
   else
   {
-    return 'F';
+    return GRADE_F;
   }
 }
 
-// Display performance stars based on grade
-void displayPerformancePattern(char grade)
+void displayPerformancePattern(Grade grade)
 {
   int stars = 0;
-
   switch (grade)
   {
-  case 'A':
-    stars = 5;
+  case GRADE_A:
+    stars = STARS_GRADE_A;
     break;
-  case 'B':
-    stars = 4;
+  case GRADE_B:
+    stars = STARS_GRADE_B;
     break;
-  case 'C':
-    stars = 3;
+  case GRADE_C:
+    stars = STARS_GRADE_C;
     break;
-  case 'D':
-    stars = 2;
+  case GRADE_D:
+    stars = STARS_GRADE_D;
     break;
   default:
-    stars = 0;
+    stars = STARS_GRADE_F;
   }
 
   for (int i = 0; i < stars; i++)
@@ -169,7 +189,6 @@ void displayPerformancePattern(char grade)
   }
 }
 
-// Recursively print roll numbers
 void printRollNumbersRecursively(StudentDetails students[], int index, int totalStudents)
 {
   if (index == totalStudents)
